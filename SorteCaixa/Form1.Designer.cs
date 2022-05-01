0a66d918f95f1df63599c633aef0fb9944395726
Felipe Souza <felipesoart@gmail.com>
2022-05-01 16:27:38 -0300

    first commit

---
+++ SorteCaixa/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SorteCaixa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSorteioSemanal = new System.Windows.Forms.Button();
+            this.lblNumerosSistema = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // btnSorteioSemanal
+            // 
+            this.btnSorteioSemanal.Location = new System.Drawing.Point(250, 38);
+            this.btnSorteioSemanal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSorteioSemanal.Name = "btnSorteioSemanal";
+            this.btnSorteioSemanal.Size = new System.Drawing.Size(144, 34);
+            this.btnSorteioSemanal.TabIndex = 0;
+            this.btnSorteioSemanal.Text = "Sorteio Semanal";
+            this.btnSorteioSemanal.UseVisualStyleBackColor = true;
+            this.btnSorteioSemanal.Click += new System.EventHandler(this.btnSorteioSemanal_Click);
+            // 
+            // lblNumerosSistema
+            // 
+            this.lblNumerosSistema.AutoSize = true;
+            this.lblNumerosSistema.Location = new System.Drawing.Point(253, 88);
+            this.lblNumerosSistema.Name = "lblNumerosSistema";
+            this.lblNumerosSistema.Size = new System.Drawing.Size(141, 17);
+            this.lblNumerosSistema.TabIndex = 1;
+            this.lblNumerosSistema.Text = "Números do sistema:";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkedListBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 38);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(58, 208);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.checkedListBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.checkedListBox2.Location = new System.Drawing.Point(89, 38);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(59, 208);
+            this.checkedListBox2.TabIndex = 3;
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.checkedListBox3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "21",
+            "22",
+            "23",
+            "24",
+            "25"});
+            this.checkedListBox3.Location = new System.Drawing.Point(168, 38);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(55, 123);
+            this.checkedListBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 263);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.lblNumerosSistema);
+            this.Controls.Add(this.btnSorteioSemanal);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSorteioSemanal;
+        private System.Windows.Forms.Label lblNumerosSistema;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+    }
+}
+
---
+++ SorteCaixa/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SorteCaixa
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSorteioSemanal_Click(object sender, EventArgs e)
+        {
+            var qntnumeros = 3268760;
+            var sorteio2 = string.Empty;
+
+            var sorteio1 = string.Empty;
+            string meuHashSet1 = string.Empty;
+            string res = string.Empty;
+            int[] sortefinal = new int[] { };
+
+            for (int i = 0; i < qntnumeros; i++)
+            {
+                    var Sorteio1 = sorteio1.Split(',');
+                    string[] SSorteio1 = Sorteio1;
+                   
+                    var meuHashSet = new HashSet<string>(SSorteio1);
+                    var valoresRepetidos = SSorteio1.Length - meuHashSet.Count;
+                    Random numAleatorio = new Random();
+                    int valorInteiro = numAleatorio.Next(1, 25);
+                   
+                    sorteio1 += valorInteiro + ",";
+                    if (meuHashSet.Count == 16)
+                    {
+                                   
+
+                    meuHashSet1 = string.Join(", ", meuHashSet.OrderBy(s => s));
+                                        
+                    string[] arrayString = meuHashSet1.Split(',');
+                    arrayString[0] = "0";
+                    int[] meuHashSet1Int = arrayString.Select(int.Parse).ToArray();
+                    
+                    sortefinal =  insertionSort(meuHashSet1Int);
+
+                    break;
+                    }
+
+            }
+
+            int indexToRemove = 0;
+            sortefinal = sortefinal.Where((source, index) => index != indexToRemove).ToArray();
+          
+            string result = string.Join(" - ", sortefinal);
+            lblNumerosSistema.Text = "Números do Sistema:\n" + result;
+
+            
+        }
+              
+        public static int[] insertionSort(int[] vetor)
+                {
+                    int i, j, atual;
+                    for (i = 1; i < vetor.Length; i++)
+                    {
+                        atual = vetor[i];
+                        j = i;
+                        while ((j > 0) && (vetor[j - 1] > atual))
+                {
+                        vetor[j] = vetor[j - 1];
+                        j = j - 1;
+                    }
+                    vetor[j] = atual;
+                }
+            return vetor;
+        }
+
+        /* https://vestibular.uol.com.br/revisao-de-disciplinas/matematica/faca-calculos-para-acertar-a-megasena.jhtm
+            chance de acertar é de 1 em 50.063.860.
+
+            Se apostarmos 1 jogo de seis dezenas, a probabilidade de ganharmos é de 1 em 50.063.860, que corresponde a 
+            0,000002% de chance de ganhar.
+
+            E = ser sorteada sena com seis números consecutivos.
+            F = ser sorteada sena que não tem números consecutivos.
+            A probabilidade de ocorrer F é muito maior do que a de ocorrer E, pois E é formado por apenas 55 senas({1, 2, 3, 4, 5, 6",
+            {2, 3, 4, 5, 6, 7", {3, 4, 5, 6, 7, 8", ..., {55, 56, 57, 58, 59, 60"), enquanto F é formado por muito mais.
+
+
+            lotofacil = 3.268.760
+            A chance de ganhar é de uma em 3.268.760. Quando analisamos os níveis secundários, o cenário é mais animador. Uma aposta de 15 
+            dezenas tem chance de vitória de uma em 21.791 na faixa de 14 acertos, já com 13 pontos, a probabilidade vai para apenas uma em 691.
+            
+            */
+
+    }
+}
